6c492cc187f71f8563193447d4e623cab19e3884
Joshua Hoban <joshua.hoban@countersight.co>
2023-03-12 21:36:02 +1100

    Implement button to swith to form

--- App.tsx
+++ App.tsx
@@ -5,20 +5,26 @@ import { Main_screen } from './components/screens/Main_screen';
 
 //React native stack navigator
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
 import Countdown_goal_form from "./components/screens/Countdown_goal_form";
+import { RouteProp, NavigationProp } from '@react-navigation/native';
+
+export type RootStackParamList = {
+  Main_screen: undefined;
+  Goal_form: undefined;
+};
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <EuiProvider colorMode="dark">
         <Stack.Navigator initialRouteName="Main_screen">
+      <EuiProvider colorMode="dark">
           <Stack.Screen name="Main_screen" component={Main_screen} />
-          <Stack.Screen name="Countdown_Goal_Form" component={Countdown_goal_form} />
-        </Stack.Navigator>
+          <Stack.Screen name="Goal_form" component={Countdown_goal_form} />
       </EuiProvider>
+        </Stack.Navigator>
     </NavigationContainer>
   );
 }
